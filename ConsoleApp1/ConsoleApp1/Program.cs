fb43ac923eb4c26aef6b5ea208a9e864eba8fe97
duybeoo <nqd3092@gmail.com>
2022-10-26 18:41:34 +0700

    Add hello 2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,6 +4,10 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        static void Main2(string[] args)
+        {
+            Console.WriteLine("Hello World 2!");
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
